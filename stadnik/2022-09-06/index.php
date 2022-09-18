3f5c788f07b7eb3eaa4642d1ef8ae244ed5b1e2f
Plasny <git.plasny.uq95y@slmail.me>
2022-09-18 15:01:01 +0200

    repo update on weekend

---
+++ stadnik/2022-09-06/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>2022-09-06</title>
+</head>
+<body>
+    <?php
+        print("<i>PHP</i> >>> <b>HTML</b><br>");
+        // echo("test");
+        print("Konkatenacja"." stringów<br>");
+
+        $test = "123<br>";
+        echo($test);
+
+        define("PI",3.1415);
+        $r = 3; 
+        echo("Pole koła: ".PI*$r**2);
+    ?>
+</body>
+</html>
