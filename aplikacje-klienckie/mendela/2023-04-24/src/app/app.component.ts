edfcfea0b26c488d84340b89e04e4e24e3a34e64
Plasny <git.plasny.uq95y@slmail.me>
2023-05-29 16:47:59 +0200

    updating o/x opponent algorithm

--- aplikacje-klienckie/mendela/2023-04-24/src/app/app.component.ts
+++ aplikacje-klienckie/mendela/2023-04-24/src/app/app.component.ts
@@ -15,6 +15,19 @@ type field = {
   dontcheck: boolean;
 };
 
+type lineParams = {
+  value: number;
+  first?: point;
+  last?: point;
+};
+
+type moveParams = {
+  horizontal: lineParams;
+  vertical: lineParams;
+  diagonal1: lineParams;
+  diagonal2: lineParams;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -58,32 +71,135 @@ export class AppComponent {
     this.board[y][x] = { type: this.player.type, dontcheck: false };
 
     console.log('players move - x:', x, 'y:', y);
-    this.check(x, y);
+    const moveParams = this.check(x, y);
 
     if (!this.playing) return;
 
-    this.aiMove(x, y);
+    this.aiMove(x, y, moveParams);
   }
 
-  // to upgrade
-  aiMove(pMoveX: number, pMoveY: number) {
+  aiMove(pMoveX: number, pMoveY: number, moveParams: moveParams) {
     this.currentPlayer = 1;
 
+    let key: string | boolean;
     let x: number;
     let y: number;
     let i = 1;
 
-    while (true) {
-      if (i < 5) {
-        x = Math.floor(pMoveX - Math.random() * 3 + 2);
-        y = Math.floor(pMoveY - Math.random() * 3 + 2);
-      } else {
-        x = Math.floor(Math.random() * this.boardWidth);
-        y = Math.floor(Math.random() * this.boardHeight);
-      }
+    if ((key = !Object.values(moveParams).every((v) => v.value === 0))) {
+      key = Object.keys(moveParams).reduce((a, b) =>
+        // @ts-ignore
+        moveParams[a].value > moveParams[b].value ? a : b
+      );
+    }
 
-      if (this.board[y][x].type === '') break;
-      i++;
+    try {
+      switch (key) {
+        // @ts-expect-error
+        case 'horizontal':
+          const tileToTheLeft =
+            this.board[moveParams.horizontal.first!.y][moveParams.horizontal.first!.x - 1];
+          const tileToTheRight =
+            this.board[moveParams.horizontal.last!.y][moveParams.horizontal.last!.x + 1];
+          y = moveParams.horizontal.first!.y;
+
+          if (tileToTheLeft !== undefined && tileToTheLeft.type === '') {
+            x = moveParams.horizontal.first!.x - 1;
+            console.warn('horizontal left');
+            break;
+          }
+          if (tileToTheRight !== undefined && tileToTheRight.type === '') {
+            x = moveParams.horizontal.last!.x + 1;
+            console.warn('horizontal right');
+            break;
+          }
+        // @ts-expect-error
+        case 'vertical':
+          const tileOnTop =
+            this.board[moveParams.vertical.first!.y - 1][moveParams.vertical.first!.x];
+          const tileOnBottom =
+            this.board[moveParams.vertical.last!.y + 1][moveParams.vertical.last!.x];
+          x = moveParams.vertical.first!.x;
+
+          if (tileOnTop !== undefined && tileOnTop.type === '') {
+            y = moveParams.vertical.first!.y - 1;
+            console.warn('vertical top');
+            break;
+          }
+          if (tileOnBottom !== undefined && tileOnBottom.type === '') {
+            y = moveParams.vertical.last!.y + 1;
+            console.warn('vertical bottom');
+            break;
+          }
+        // @ts-expect-error
+        case 'diagonal1':
+          const begginingOfTiles1 =
+            this.board[moveParams.diagonal1.first!.y - 1][moveParams.diagonal1.first!.x - 1];
+          const endOfTiles1 =
+            this.board[moveParams.diagonal1.last!.y + 1][moveParams.diagonal1.last!.x + 1];
+
+          if (
+            begginingOfTiles1 !== undefined &&
+            begginingOfTiles1.type === ''
+          ) {
+            x = moveParams.diagonal1.first!.x - 1;
+            y = moveParams.diagonal1.first!.y - 1;
+            console.warn('diagonal1 start');
+            break;
+          }
+          if (endOfTiles1 !== undefined && endOfTiles1.type === '') {
+            x = moveParams.diagonal1.last!.x + 1;
+            y = moveParams.diagonal1.last!.y + 1;
+            console.warn('diagonal1 end');
+            break;
+          }
+        // @ts-expect-error
+        case 'diagonal2':
+          const begginingOfTiles2 =
+            this.board[moveParams.diagonal2.first!.y + 1][moveParams.diagonal2.first!.x - 1];
+          const endOfTiles2 =
+            this.board[moveParams.diagonal2.last!.y - 1][moveParams.diagonal2.last!.x + 1];
+
+          if (
+            begginingOfTiles2 !== undefined &&
+            begginingOfTiles2.type === ''
+          ) {
+            x = moveParams.diagonal2.first!.x - 1;
+            y = moveParams.diagonal2.first!.y + 1;
+            console.warn('diagonal2 start');
+            break;
+          }
+          if (endOfTiles2 !== undefined && endOfTiles2.type === '') {
+            x = moveParams.diagonal2.last!.x + 1;
+            y = moveParams.diagonal2.last!.y - 1;
+            console.warn('diagonal2 end');
+            break;
+          }
+        default:
+          // todo move code from catch here
+          throw "default"
+      }
+    } catch {
+      // lazy fix -> to refactor
+      while (true) {
+        if (i < 10) {
+          x = Math.floor(pMoveX - Math.random() * 3 + 2);
+          y = Math.floor(pMoveY - Math.random() * 3 + 2);
+        } else {
+          x = Math.floor(Math.random() * this.boardWidth);
+          y = Math.floor(Math.random() * this.boardHeight);
+        }
+
+        if (
+          x >= 0 &&
+          y >= 0 &&
+          x < this.boardWidth &&
+          y < this.boardHeight &&
+          this.board[y][x].type === ''
+        )
+          break;
+        i++;
+      }
     }
 
     this.board[y][x] = { type: this.opponent.type, dontcheck: false };
@@ -95,11 +211,24 @@ export class AppComponent {
     this.currentPlayer = 0;
   }
 
-  check(x: number, y: number) {
-    console.groupCollapsed('check');
-    let i: number, start: number, end: number, x1: number, y1: number;
-    let count: number;
+  check(x: number, y: number): moveParams {
     let currentType: field['type'];
+
+    let moveParams: moveParams = {
+      horizontal: { value: -1 },
+      vertical: { value: -1 },
+      diagonal1: { value: -1 },
+      diagonal2: { value: -1 },
+    };
+
+    let i: number,
+      start: number,
+      end: number,
+      x1: number,
+      y1: number,
+      count: number;
+
+    console.groupCollapsed('check');
 
     // -------------------- horizontal (-) check --------------------
     console.log('(-) horizontal:');
@@ -118,8 +247,15 @@ export class AppComponent {
       }
 
       if (currentType === this.board[y][i].type && currentType != '') {
+        if (moveParams.horizontal.first === undefined) {
+          moveParams.horizontal.first = { x: i - 1, y: y };
+        }
         count++;
       } else {
+        if (moveParams.horizontal.value < count) {
+          moveParams.horizontal.value = count;
+          moveParams.horizontal.last = { x: i - 1, y: y };
+        }
         if (count >= 4) break;
         count = 0;
       }
@@ -152,8 +288,15 @@ export class AppComponent {
       }
 
       if (currentType === this.board[i][x].type && currentType != '') {
+        if (moveParams.vertical.first === undefined) {
+          moveParams.vertical.first = { x: x, y: i - 1 };
+        }
         count++;
       } else {
+        if (moveParams.vertical.value < count) {
+          moveParams.vertical.value = count;
+          moveParams.vertical.last = { x: x, y: i - 1 };
+        }
         if (count >= 4) break;
         count = 0;
       }
@@ -195,6 +338,11 @@ export class AppComponent {
         count++;
         points.push({ x: x1, y: y1 });
       } else {
+        if (moveParams.diagonal1.value < count) {
+          moveParams.diagonal1.value = count;
+          moveParams.diagonal1.first = points[0];
+          moveParams.diagonal1.last = points[points.length - 1];
+        }
         if (count >= 4) break;
         count = 0;
         points = [{ x: x1, y: y1 }];
@@ -238,9 +386,12 @@ export class AppComponent {
         count++;
         points.push({ x: x1, y: y1 });
       } else {
-        if (count >= 4) {
-          break;
+        if (moveParams.diagonal2.value < count) {
+          moveParams.diagonal2.value = count;
+          moveParams.diagonal2.first = points[0];
+          moveParams.diagonal2.last = points[points.length - 1];
         }
+        if (count >= 4) break;
         count = 0;
         points = [{ x: x1, y: y1 }];
       }
@@ -258,6 +409,7 @@ export class AppComponent {
       this.p[this.currentPlayer].points++;
     }
 
+    console.log('summary:', moveParams);
     console.groupEnd();
 
     if (this.player.points >= this.maxPoints) {
@@ -268,5 +420,7 @@ export class AppComponent {
       alert('You lost!');
       this.playing = false;
     }
+
+    return moveParams;
   }
 }
